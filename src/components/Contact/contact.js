ba6112745833567cc58f4e5bbe622506ba60f476
Klemen Kocic <Klemen@seascape.network>
2024-02-10 20:01:47 +0100

    contact form

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./contact.css";
+import emailjs from '@emailjs/browser';
 
 import FacebookIcon from "../../assets/facebook-icon.png";
 import TwitterIcon from "../../assets/twitter.png";
@@ -7,16 +8,29 @@ import InstagramIcon from "../../assets/instagram.png";
 import YoutubeIcon from "../../assets/youtube.png";
 
 const Contact = () => {
+    const form = useRef();
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('service_t8vmwih', 'template_86bco38', form.current, 'vcropGLpTix63_9af')
+            .then((result) => {
+                console.log('SUCCESS!', result.text);
+                alert("Email Sent!");
+                e.target.reset();
+            }, (error) => {
+                console.log('FAILED...', error.text);
+            });
+    };
+
     return (
         <section id="contactPage">
             <div id="contact">
                 <h1 className="contactPageTitle">Contact Me</h1>
                 <span className="contactDesc">Please fill out the form below to discuss any work opportunities</span>
-                <form className="contactForm">
-                    <input type="text" className="name" placeholder="Your Name"/>
-                    <input type="email" className="email" placeholder="Your Email"/>
+                <form className="contactForm" ref={form} onSubmit={sendEmail}>
+                    <input type="text" className="name" placeholder="Your Name" name="your_name"/>
+                    <input type="email" className="email" placeholder="Your Email" name="your_email"/>
                     <textarea className="msg" name="message" rows="5" placeholder="Your Message"></textarea>
-                    <button type="sumbit" value="Send" className="submitBtn">Submit</button>
+                    <button type="submit" value="Send" className="submitBtn">Submit</button>
                     <div className="links">
                         <img src={FacebookIcon} alt="Facebook" className="link"/>
                         <img src={TwitterIcon} alt="Twitter" className="link"/>
